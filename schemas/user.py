6fc1803ce342eb0da785dfde343f145b455662c4
zero1max <nigmatjonovmuhammadjon08@gmail.com>
2025-04-09 10:14:18 +0500

    Update user

--- schemas/user.py
+++ schemas/user.py
@@ -4,3 +4,15 @@ class BaseUser(BaseModel):
     email: EmailStr
     username: str
     password: str
+
+    class Config:
+        orm_mode = True
+
+
+class UserOut(BaseModel):
+    id: int
+    email: str
+    username: str
+
+    class Config:
+        from_attributes = True 
